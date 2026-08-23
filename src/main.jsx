5ea883e6486afb7044c4772dcbeea80fb9a5ac87
bikeztagram-ai <dsengineeringhull@gmail.com>
2026-08-23 17:16:51 +0100

    feat: mount Creative Director command centre

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import OutputFormatEnhancer from './outputFormatEnhancer.jsx';
+import CreativeCommandCenter from './creativeCommandCenter.jsx';
 import './styles.css';
 import './qa.js';
 import './exportTools.js';
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <OutputFormatEnhancer />
+    <CreativeCommandCenter />
   </React.StrictMode>
 );
